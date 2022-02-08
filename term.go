b650635dc883bedcaccc34873b0c2a57d0bc337a
Riboloff <makcimgovorov@gmail.com>
2022-02-08 19:15:03 +0300

    move char and arm up

--- term.go
+++ term.go
@@ -7,7 +7,7 @@ import (
 )
 
 type char struct {
-	head, body, legs string
+	head, body, legs [3]byte
 	cord             cord
 	height           int
 }
@@ -17,7 +17,6 @@ type cord struct {
 }
 
 type _map struct {
-	//grid [2][8]byte
 	grid [100][4]byte
 }
 
@@ -33,22 +32,20 @@ func printChar(char char) {
 }
 
 func charArmUp(m *char) {
-	m.head = " 0/"
-	m.body = "<# "
-	m.legs = "/ \\"
+	m.head[2] = '/'
+	m.body[2] = ' '
 }
 
 func charArmDown(m *char) {
-	m.head = " 0 "
-	m.body = "<#\\"
-	m.legs = "/ \\"
+	m.head[2] = ' '
+	m.body[2] = '\\'
 }
 
 func charInit() char {
 	return char{
-		head:   " 0 ",
-		body:   "<#\\",
-		legs:   "/ \\",
+		head:   [3]byte{' ', '0', ' '},
+		body:   [3]byte{'<', '#', '\\'},
+		legs:   [3]byte{'/', ' ', '\\'},
 		cord:   cord{1, 1},
 		height: 3}
 }
@@ -67,8 +64,34 @@ func mapInit() _map {
 func putCharOnMap(m *_map, char char) {
 	cord := char.cord
 
-	for i := 0; i < len(g); i++ {
-		for j := 0; j < len(g[0]); j++ {
+	flag := 0
+	for i := 0; i < len(m.grid); i++ {
+		for j := 0; j < len(m.grid[0]); j++ {
+			if cord.x == i && cord.y == j {
+				putChar(i, j, m, char)
+				flag = 1
+				break
+			}
+		}
+		if flag == 1 {
+			break
+		}
+	}
+}
+
+func putChar(x int, y int, m *_map, char char) {
+
+	for i := 0; i < char.height; i++ {
+		for j := 0; j < len(char.head); j++ {
+			if i == 0 {
+				m.grid[x+j][y+i] = char.head[j]
+			}
+			if i == 1 {
+				m.grid[x+j][y+i] = char.body[j]
+			}
+			if i == 2 {
+				m.grid[x+j][y+i] = char.legs[j]
+			}
 		}
 	}
 }
@@ -89,21 +112,23 @@ func main() {
 	tm.Clear()
 	char := charInit()
 	_map := mapInit()
-	printMap(_map, char)
+	//printMap(_map, char)
+
 	//charDance(char)
-	//charMoveRight(char)
+	charMoveRight(_map, char)
 }
 
-func charMoveRight(char char) {
+func charMoveRight(m _map, char char) {
 	i := 0
 	for {
-		char.cord.x = char.cord.x + 3
-		tm.MoveCursor(char.cord.x, char.cord.y)
+		char.cord.x = char.cord.x + 1
 
-		printChar(char)
-		//if i%2 == 1 {
-		//} else {
-		//}
+		printMap(m, char)
+		if i%2 == 1 {
+			charArmUp(&char)
+		} else {
+			charArmDown(&char)
+		}
 		time.Sleep(time.Second)
 		i = i + 1
 	}
