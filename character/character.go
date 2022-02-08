5ecf96acb4fb1504b106b4818e52e601f5faee2e
Riboloff <makcimgovorov@gmail.com>
2022-02-08 23:15:19 +0300

    add move right

--- character/character.go
+++ character/character.go
@@ -1,13 +1,30 @@
 package character
 
+const (
+	FRONT = iota + 1
+	RIGHT_ONE
+	RIGHT_TWO
+)
+
 type Char struct {
 	Head, Body, Legs [3]byte
 	Cord             Cord
 	Height           int
+	State            int
 }
 
 type Cord struct {
 	X, Y int
+}
+
+func NewChar() Char {
+	return Char{
+		Head:   [3]byte{' ', '0', ' '},
+		Body:   [3]byte{'<', '#', '\\'},
+		Legs:   [3]byte{'/', ' ', '\\'},
+		Cord:   Cord{1, 1},
+		State:  FRONT,
+		Height: 3}
 }
 
 func (c *Char) ArmUp() {
@@ -20,11 +37,22 @@ func (c *Char) ArmDown() {
 	c.Body[2] = '\\'
 }
 
-func NewChar() Char {
-	return Char{
-		Head:   [3]byte{' ', '0', ' '},
-		Body:   [3]byte{'<', '#', '\\'},
-		Legs:   [3]byte{'/', ' ', '\\'},
-		Cord:   Cord{1, 1},
-		Height: 3}
+func (c *Char) MoveRight() {
+	c.Cord.X = c.Cord.X + 1
+
+	if c.State != RIGHT_ONE {
+		c.State = RIGHT_ONE
+		c.Body[0] = '.'
+		c.Body[2] = '\\'
+		c.Legs[0] = '/'
+		c.Legs[1] = '>'
+		c.Legs[2] = ' '
+	} else {
+		c.State = RIGHT_TWO
+		c.Body[0] = '<'
+		c.Body[2] = '.'
+		c.Legs[0] = '-'
+		c.Legs[1] = '|'
+		c.Legs[2] = ' '
+	}
 }
--- term.go
+++ term.go
@@ -5,61 +5,34 @@ import (
 
 	"term/character"
 	"term/gamemap"
-
-	tm "github.com/buger/goterm"
+	"term/printer"
 )
 
-func putCharOnMap(m *gamemap.Map, char character.Char) {
-	x := char.Cord.X
-	y := char.Cord.Y
-
-	for i := 0; i < char.Height; i++ {
-		for j := 0; j < len(char.Head); j++ {
-			switch i {
-			case 0:
-				m.Grid[x+j][y+i] = char.Head[j]
-			case 1:
-				m.Grid[x+j][y+i] = char.Body[j]
-			case 2:
-				m.Grid[x+j][y+i] = char.Legs[j]
-			}
-		}
-	}
-}
-
-func printMap(m gamemap.Map, char character.Char) {
-	putCharOnMap(&m, char)
-	g := m.Grid
-	for i := 1; i < len(g); i++ {
-		for j := 1; j < len(g[0]); j++ {
-			tm.MoveCursor(i, j)
-			tm.Printf("%c", g[i][j])
-		}
-	}
-	tm.Flush()
-}
-
 func main() {
-	tm.Clear()
+	printer.Clear()
 	char := character.NewChar()
-	_map := gamemap.NewMap()
-	//printMap(_map, char)
+	gamemap := gamemap.NewMap()
 
-	charMoveRight(_map, char)
+	charDancing(gamemap, char, 3)
+	charMoveRight(gamemap, char)
 }
 
 func charMoveRight(m gamemap.Map, char character.Char) {
-	i := 0
 	for {
-		char.Cord.X = char.Cord.X + 1
+		char.MoveRight()
+		printer.PrintMap(m, char)
+		time.Sleep(time.Second)
+	}
+}
 
-		printMap(m, char)
-		if i%2 == 1 {
+func charDancing(m gamemap.Map, char character.Char, count int) {
+	for i := 0; i < count; i++ {
+		printer.PrintMap(m, char)
+		if i%2 == 0 {
 			char.ArmUp()
 		} else {
 			char.ArmDown()
 		}
 		time.Sleep(time.Second)
-		i = i + 1
 	}
 }
